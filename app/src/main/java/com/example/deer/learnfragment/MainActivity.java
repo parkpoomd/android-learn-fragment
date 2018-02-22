42eb4d13a057c34b8d127d96cfc882f0007aee00
parkpoomd <parkpoom.d@gmail.com>
2018-02-22 10:13:32 +0700

    git init first projects

---
+++ app/src/main/java/com/example/deer/learnfragment/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.deer.learnfragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_my_fragment);
+        MyFragment myFragment = (MyFragment) fragment;
+    }
+}
