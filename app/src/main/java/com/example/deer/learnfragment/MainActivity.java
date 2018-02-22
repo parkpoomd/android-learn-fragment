02847ae0a9ec4686d38ec383ecdc34334c843a44
parkpoomd <parkpoom.d@gmail.com>
2018-02-22 13:38:12 +0700

    imprement fragment-transaction 1

--- app/src/main/java/com/example/deer/learnfragment/MainActivity.java
+++ app/src/main/java/com/example/deer/learnfragment/MainActivity.java
@@ -3,15 +3,58 @@ package com.example.deer.learnfragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnAddFragment;
+    private Button btnRemoveFragment;
+
+    private static final String TAG_HOME_FRAGMENT = "tag_home_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_my_fragment);
-        MyFragment myFragment = (MyFragment) fragment;
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.layout_fragment_container, HomeFragment.newInstance())
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+        btnAddFragment = (Button) findViewById(R.id.btn_add_fragment);
+        btnRemoveFragment = (Button) findViewById(R.id.btn_remove_fragment);
+
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Add fragment on ViewGroup(FrameLayout)
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.layout_fragment_container,
+                                HomeFragment.newInstance(),
+                                TAG_HOME_FRAGMENT)
+                        .commit();
+            }
+        });
+
+        btnRemoveFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Remove fragment on ViewGroup(FrameLayout)
+
+                Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG_HOME_FRAGMENT);
+                if (fragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .remove(fragment)
+                            .commit();
+                }
+
+            }
+        });
     }
 }
